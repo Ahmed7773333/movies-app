924a5e70981b6232c856f3b34ee49bf91800b452
HatemReda7 <hreda2567@gmail.com>
2023-10-29 15:10:04 +0200

    added APi key

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,2 @@
 const String apiKeyHatem = "fbd23ec55c10dc8d023dcc189a1108c5";
 const String apiKeyAhmed = '40e534d570c8ebe333fe3e7667baf641';
-const String baseUrl= "api.themoviedb.org";
-const String authorizationAccessToken= "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYmQyM2VjNTVjMTBkYzhkMDIzZGNjMTg5YTExMDhjNSIsInN1YiI6IjY1M2QyMzYyNTkwN2RlMDBmZTFlOGRkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KnljD1cNNEAlXqZtSZ2T30hmLoYayp_fTJ3-hftm5K0";
