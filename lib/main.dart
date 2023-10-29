4c3b5a66fd84c88b58c44de386de5d390465a3f2
HatemReda7 <hreda2567@gmail.com>
2023-10-29 15:08:22 +0200

    working on problem

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,16 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movies_app/config/themes.dart';
 import 'features/bottom bar screen/presentation/bottom_bar.dart';
 import 'features/splach screen/splach_screen.dart';
+import 'dart:io';
+import 'package:hive/hive.dart' as hive;
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory directory = await getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
   runApp(const MyApp());
 }
 
