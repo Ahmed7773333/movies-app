5719dcdf4f0991a5e4baa9cffc1f37a648456c4e
HatemReda7 <hreda2567@gmail.com>
2023-11-01 21:21:59 +0200

    edited search list

--- lib/features/watsh list tab/presentation/widgets/watch_listItem.dart
+++ lib/features/watsh list tab/presentation/widgets/watch_listItem.dart
@@ -28,62 +28,65 @@ class _WatchListItemState extends State<WatchListItem> {
   @override
   Widget build(BuildContext context) {
     return OpenContainers(
-        closedWidget: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              children: [
-                Stack(
-                  children: [
-                    SizedBox(
-                      width: 96.87.w,
-                      height: 100.74.h,
-                      child: Image.network(
-                          "https://image.tmdb.org/t/p/w500/${widget.movie.posterPath}"),
-                    ),
-                    Positioned(
-                        left: 0,
-                        top: 0,
-                        child: InkWell(
-                          onTap: () {
-                            ApiManager.deleteFromWatchlist(widget.movie);
-                            debugPrint('working');
-                            setState(() {});
-                          },
-                          child: SizedBox(
-                              width: 27.w,
-                              height: 36.h,
-                              child: Image.asset(booked)),
-                        ))
-                  ],
-                ),
-                const HorizontalSpace(5),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+        closedWidget: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  Stack(
                     children: [
-                      RichText(text: TextSpan(text: widget.movie.title ?? "")),
-                      const VerticalSpace(5),
-                      Text(
-                        widget.movie.releaseDate ?? '',
-                        style: smallText2,
+                      SizedBox(
+                        width: 140.w,
+                        height: 89.h,
+                        child: Image.network(
+                            "https://image.tmdb.org/t/p/w500/${widget.movie.posterPath}",fit: BoxFit.fill,),
                       ),
-                      const VerticalSpace(5)
+                      Positioned(
+                          left: 0,
+                          top: 0,
+                          child: InkWell(
+                            onTap: () {
+                              ApiManager.deleteFromWatchlist(widget.movie);
+                              debugPrint('working');
+                              setState(() {});
+                            },
+                            child: SizedBox(
+                                width: 27.w,
+                                height: 36.h,
+                                child: Image.asset(booked)),
+                          ))
                     ],
                   ),
-                )
-              ],
-            ),
-            const VerticalSpace(15),
-            Divider(
-              color: Colors.white,
-              indent: 20.5.w,
-              endIndent: 33.5.w,
-              height: 1,
-            ),
-            const VerticalSpace(25),
-          ],
+                  const HorizontalSpace(5),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(text: TextSpan(text: widget.movie.title ?? "")),
+                        const VerticalSpace(5),
+                        Text(
+                          widget.movie.releaseDate?.substring(0,4) ?? '',
+                          style: smallText2,
+                        ),
+                        const VerticalSpace(5)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const VerticalSpace(15),
+              Divider(
+                color: Colors.white,
+                indent: 20.5.w,
+                endIndent: 33.5.w,
+                height: 1,
+              ),
+              const VerticalSpace(25),
+            ],
+          ),
         ),
         openedWidget: MovieDetailsScreen(movie: widget.movie));
   }
