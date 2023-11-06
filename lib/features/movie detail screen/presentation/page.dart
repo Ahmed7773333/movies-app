8c4fe70c6c59ac0e264a4a08c835f7c95ca4c1b6
HatemReda7 <hreda2567@gmail.com>
2023-11-06 16:04:45 +0200

    edited science fiction name

--- lib/features/movie detail screen/presentation/page.dart
+++ lib/features/movie detail screen/presentation/page.dart
@@ -232,7 +232,7 @@ class MovieDetailsScreen extends StatelessWidget {
     {"id": 10402, "name": "Music"},
     {"id": 9648, "name": "Mystery"},
     {"id": 10749, "name": "Romance"},
-    {"id": 878, "name": "Science Fiction"},
+    {"id": 878, "name": "Sci-Fi"},
     {"id": 10770, "name": "TV Movie"},
     {"id": 53, "name": "Thriller"},
     {"id": 10752, "name": "War"},
